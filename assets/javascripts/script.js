4fe1ced936132ecb407ca55a2362bf95e6fa3ea6
Christian Eric Cosca <ccosca@iripple.com>
2025-07-18 21:42:55 +0800

    Separate concerns for sketch and draw methods

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -3,7 +3,7 @@ const MIN_GRID_SIZE = 16;
 const MAX_GRID_SIZE = 100;
 
 // Global Variables
-let userGridSize, colorMethod, alpha, color='0,0,0';
+let userGridSize, colorMethod, alpha, color = '0,0,0';
 
 // Containers
 const gridContainer = document.querySelector('.grid-container');
@@ -11,20 +11,21 @@ const gridContainer = document.querySelector('.grid-container');
 // Buttons and Inputs
 const newGridBtn = document.querySelector('.new-grid-btn');
 const clearBtn = document.querySelector('.clear-btn');
-clearBtn.setAttribute('disabled','');
+clearBtn.setAttribute('disabled', '');
 const randomCanvasBtn = document.querySelector('.random-canvas');
 randomCanvasBtn.setAttribute('disabled', '');
 const randomColorCheckBox = document.querySelector('#random-color');
-randomColorCheckBox.setAttribute('disabled','');
+randomColorCheckBox.setAttribute('disabled', '');
 const opacityCheckBox = document.querySelector('#opacity');
-opacityCheckBox.setAttribute('disabled','');
+opacityCheckBox.setAttribute('disabled', '');
 const colorInput = document.querySelector('#color');
-colorInput.setAttribute('disabled','');
+colorInput.setAttribute('disabled', '');
 
+// Methods and Event Listeners
 function createRow(gridSize) {
     const gridRow = document.createElement('div');
     gridRow.classList.add('row');
-    for(let cols = 0; cols < gridSize; cols++) {
+    for (let cols = 0; cols < gridSize; cols++) {
         const gridColumn = document.createElement('div');
         gridColumn.classList.add('cell');
         gridRow.appendChild(gridColumn);
@@ -33,66 +34,73 @@ function createRow(gridSize) {
 }
 
 function createGrid(grid, gridSize) {
-    for(let row = 0; row < gridSize; row++) {
+    for (let row = 0; row < gridSize; row++) {
         grid.appendChild(createRow(gridSize));
     }
 }
 
 function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? [
-    parseInt(result[1], 16),
-    parseInt(result[2], 16),
-    parseInt(result[3], 16)
-   ].join(',') : null;
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+    ].join(',') : null;
 }
 
 function randomBgColor() {
-    const [v1,v2,v3] = [
-        Math.floor(Math.random()*256),
-        Math.floor(Math.random()*256),
-        Math.floor(Math.random()*256)
+    const [v1, v2, v3] = [
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256)
     ];
-    return [v1,v2,v3].join(',');
+    return [v1, v2, v3].join(',');
 }
 
 colorInput.addEventListener('change', e => {
     color = hexToRgb(e.target.value);
 });
 
+gridContainer.addEventListener('mouseenter', () => { 
+    alpha = 0.1;
+});
+
+function sketch(row) {
+    row.addEventListener('mouseover', e => {
+        colorMethod = randomColorCheckBox.checked ? randomBgColor() : color;
+        if (opacityCheckBox.checked) {
+            e.target.style.backgroundColor = `rgba(${colorMethod},${Number(alpha.toFixed(2))})`;
+            if (Number(alpha.toFixed(2)) < 1.0) alpha += 0.1;
+        }
+        else {
+            e.target.style.backgroundColor = `rgb(${colorMethod})`;
+        }
+    });
+}
+
 function draw(grid) {
-    if(grid.children) {
+    if (grid.children) {
         const rows = Array.from(grid.children);
-        gridContainer.addEventListener('mouseenter', () => { alpha = 0.1; });
         rows.forEach(row => {
-            row.addEventListener('mouseover', e => {
-                colorMethod = randomColorCheckBox.checked ? randomBgColor() : color;
-                if(opacityCheckBox.checked) {
-                    e.target.style.backgroundColor = `rgba(${colorMethod},${Number(alpha.toFixed(2))})`;
-                    if(Number(alpha.toFixed(2)) < 1.0) alpha += 0.1;
-                }
-                else {
-                    e.target.style.backgroundColor = `rgb(${colorMethod})`;
-                }
-            });
+            sketch(row);
         });
     }
 }
 
 function clear(grid) {
-    if(grid.children) {
+    if (grid.children) {
         const rows = Array.from(grid.children);
         rows.forEach(row => {
-            [...row.children].forEach(child=>{child.style.backgroundColor='';});
+            [...row.children].forEach(child => { child.style.backgroundColor = ''; });
         });
     }
 }
 
 function randomCanvas(grid) {
-    if(grid.children) {
+    if (grid.children) {
         const rows = Array.from(grid.children);
         rows.forEach(row => {
-            [...row.children].forEach(child=>{child.style.backgroundColor=`rgb(${randomBgColor()})`;});
+            [...row.children].forEach(child => { child.style.backgroundColor = `rgb(${randomBgColor()})`; });
         });
     }
 }
@@ -110,9 +118,9 @@ newGridBtn.addEventListener('click', () => {
     enableButtons();
     do {
         userGridSize = prompt(`Please enter a grid size (from ${MIN_GRID_SIZE} to ${MAX_GRID_SIZE})`);
-        if(userGridSize === null) return;
+        if (userGridSize === null) return;
         userGridSize = Number(userGridSize);
-    } while(userGridSize < MIN_GRID_SIZE || userGridSize > MAX_GRID_SIZE);
+    } while (userGridSize < MIN_GRID_SIZE || userGridSize > MAX_GRID_SIZE);
     createGrid(gridContainer, userGridSize);
     draw(gridContainer);
     clearBtn.addEventListener('click', () => {
